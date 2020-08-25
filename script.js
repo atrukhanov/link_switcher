673f56aca19da7399cc7862c4afcee9304e2b1c6
atrukhanov <>
2020-08-25 21:33:29 +0300

    delete element

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-	let comments = document.getElementById("comments");
+	let comments = document.getElementById('comments');
 	let timeCodes = comments.querySelectorAll('div:nth-child(2) > div:nth-child(1)\
 	 > div.pure-u-20-24.pure-u-md-22-24 > p:nth-child(2) > a');
 	timeCodes.forEach(elem => {
@@ -7,11 +7,10 @@ window.onload = () => {
 		let nodeTimes = elem.textContent.split(':');
 		let timeFormat = nodeTimes.reduceRight((res, curr) => {
 			return signs.pop() + curr + res;
-			}, '');
-		let link = 'potplayer://https://youtu.be/' +
+		}, '');
+		elem.href = 'potplayer://https://youtu.be/' +
 			window.location.search.split('=')[1] +
 			'?/t=' +
 			timeFormat;
-		elem.href = link;
 	});
 };
