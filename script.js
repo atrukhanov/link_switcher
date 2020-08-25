52bf967fdb73681d9ca959626949749888fec397
atrukhanov <team21.alex@gmail.com>
2020-08-25 22:28:05 +0300

    delete unuses data and fix

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ window.onload = () => {
 	let timeCodes = comments.querySelectorAll('div:nth-child(2) > div:nth-child(1)\
 	 > div.pure-u-20-24.pure-u-md-22-24 > p:nth-child(2) > a');
 	timeCodes.forEach(elem => {
-		let signs = ['h','m','s'];
 		let nodeTimes = elem.textContent.split(':');
 		let timeFormat = nodeTimes.reduceRight((res, curr, i) => {
-			return res + parseInt(curr) * (2 - i);
+			return res + parseInt(curr) * (3 - i);
 		}, 0);
 		elem.href = 'potplayer://https://youtu.be/' +
 			window.location.search.split('=')[1] +
