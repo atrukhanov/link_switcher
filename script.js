0edcb4951ba7653d574ca951d4761a4e7acaee89
Alexander <54072529+atrukhanov@users.noreply.github.com>
2020-08-26 19:38:25 +0300

    fix convert time and refactor

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 window.onload = () => {
 	let comments = document.getElementById('comments');
-	let timeCodes = comments.querySelectorAll('div:nth-child(2) > div:nth-child(1)\
-	 > div.pure-u-20-24.pure-u-md-22-24 > p:nth-child(2) > a');
+	let timeCodes = comments.querySelectorAll(
+		'div:nth-child(2) > \
+		div:nth-child(1) > \
+		div.pure-u-20-24.pure-u-md-22-24 > \
+		p:nth-child(2) > a'
+	);
 	timeCodes.forEach(elem => {
+		const id = window.location.search.split('=')[1];
 		let nodeTimes = elem.textContent.split(':');
-		let timeFormat = nodeTimes.reduceRight((res, curr, i) => {
-			return res + parseInt(curr) * (nodeTimes.length - i);
-		}, 0);
-		elem.href = 'potplayer://https://youtu.be/' +
-			window.location.search.split('=')[1] +
-			'?t=' +
-			timeFormat;
+		let timeSec = nodeTimes.reduceRight((res, curr, i) => {
+			return res + parseInt(curr) * Math.pow(60, nodeTimes.length - i - 1);
+		}, 0);		
+		elem.href = `potplayer://https://youtu.be/${id}?t=${timeSec}`;
 	});
 };
