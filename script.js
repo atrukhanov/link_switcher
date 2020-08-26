8406910defcd3cc9e4215918c8b5684342786a77
Alex <iampopovich@gmail.com>
2020-08-26 03:06:01 +0300

    codestyle update
    tabulations replaced with 2-space indentation (less pythonic code)
    elem.href now generates by joining list of values instead of series of +
    each element in XPATH now separated with their own line

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.onload = () => {
-	let comments = document.getElementById('comments');
-	let timeCodes = comments.querySelectorAll('div:nth-child(2) > div:nth-child(1)\
-	 > div.pure-u-20-24.pure-u-md-22-24 > p:nth-child(2) > a');
-	timeCodes.forEach(elem => {
-		let nodeTimes = elem.textContent.split(':');
-		let timeFormat = nodeTimes.reduceRight((res, curr, i) => {
-			return res + parseInt(curr) * (nodeTimes.length - i);
-		}, 0);
-		elem.href = 'potplayer://https://youtu.be/' +
-			window.location.search.split('=')[1] +
-			'?t=' +
-			timeFormat;
-	});
+  let comments = document.getElementById('comments');
+  let timeCodes = comments.querySelectorAll(
+    'div:nth-child(2) > \
+    div:nth-child(1) > \
+    div.pure-u-20-24.pure-u-md-22-24 > \
+    p:nth-child(2) > \
+    a'
+  );
+  timeCodes.forEach(elem => {
+    let nodeTimes = elem.textContent.split(':');
+    let timeFormat = nodeTimes.reduceRight((res, curr, i) => {
+      return res + parseInt(curr) * (nodeTimes.length - i);
+    }, 0);
+    elem.href = [
+      'potplayer://https://youtu.be/',
+      window.location.search.split('=')[1],
+      '?t=',
+      timeFormat
+      ].join('');
+  });
 };
