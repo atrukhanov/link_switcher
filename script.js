953a3f34ac332b76e5b2edb01c13b6c0ad5f10f8
Alexander <54072529+atrukhanov@users.noreply.github.com>
2020-08-28 14:32:45 +0300

    length parameter wrapped with constant value

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onload = () => {
 	timeCodes.forEach(elem => {
 		const id = window.location.search.split('=')[1];
 		let nodeTimes = elem.textContent.split(':');
+		const length = nodeTimes.length - 1;
 		let timeSec = nodeTimes.reduceRight((res, curr, i) => {
-			return res + parseInt(curr) * Math.pow(60, nodeTimes.length - i - 1);
+			return res + parseInt(curr) * Math.pow(60, length - i);
 		}, 0);
 		elem.href = `potplayer://https://youtu.be/${id}?t=${timeSec}`;
 	});
